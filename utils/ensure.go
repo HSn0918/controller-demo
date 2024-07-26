46b5f34099794b9e90a17ff1c1200bae341e90ca
HSn0918 <289744493@qq.com>
2024-07-26 21:46:27 +0800

    update: Dockerfile

--- utils/ensure.go
+++ utils/ensure.go
@@ -9,31 +9,25 @@ import (
 func EnsureMapFieldsInitializedBFS(obj interface{}) {
 	queue := list.New()
 	queue.PushBack(reflect.ValueOf(obj))
-
 	for queue.Len() > 0 {
 		elem := queue.Front()
 		v := elem.Value.(reflect.Value)
 		queue.Remove(elem)
-
 		if v.Kind() == reflect.Ptr {
 			v = v.Elem()
 		}
-
 		if v.Kind() != reflect.Struct {
 			continue
 		}
-
 		for i := 0; i < v.NumField(); i++ {
 			field := v.Field(i)
 			fieldType := field.Type()
-
-			// 检查并初始化 map 字段
 			if field.Kind() == reflect.Map && field.IsNil() {
 				field.Set(reflect.MakeMap(fieldType))
 			}
-
-			// 如果字段是结构体或指向结构体的指针，添加到队列中
-			if (field.Kind() == reflect.Struct) || (field.Kind() == reflect.Ptr && field.Elem().Kind() == reflect.Struct) {
+			if (field.Kind() == reflect.Struct) ||
+				(field.Kind() == reflect.Ptr &&
+					field.Elem().Kind() == reflect.Struct) {
 				queue.PushBack(field)
 			}
 		}
